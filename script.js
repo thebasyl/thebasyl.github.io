bdd0d40419c62d7f5e874b0780b7247c1d30f33b
thebasyl <noel@nimstad.com>
2022-10-19 21:50:51 +0200

    added upcoming box and fixed some animation issues

--- script.js
+++ script.js
@@ -8,5 +8,7 @@ const button2 = document.getElementById("button2");
 
 button2.addEventListener("click", () => 
 {
+    console.log(boxes.childNodes)
     boxes.childNodes[1].childNodes[1].classList.toggle("hidden");
+    boxes.childNodes[3].childNodes[1].classList.toggle("hidden");
 })
