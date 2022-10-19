eebf1ace365ec994e12d89cbaf307c50c4bd2393
thebasyl <noel@nimstad.com>
2022-10-19 17:46:19 +0200

    added cool 3d game boxes with an epic exit and enter animation powered by scss and cubic bezier curves. also random background colours

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.getElementById("elevator-button").addEventListener("click", () => 
-{
+const colours = ["lightblue", "lightpink", "lightsalmon", "lightgreen", "lightseagreen"];
+const randomColour = colours[Math.floor(Math.random() * colours.length)];
 
+document.body.style.backgroundColor = randomColour;
+
+const boxes = document.getElementById("game-boxes");
+const button2 = document.getElementById("button2");
+
+button2.addEventListener("click", () => 
+{
+    boxes.childNodes[1].childNodes[1].classList.toggle("hidden");
 })
